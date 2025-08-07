d6bdc0918be6c670e6ff4fc26a77a6390c9c21e8
LachubothuRakesh <143777530+LachubothuRakesh@users.noreply.github.com>
2025-08-07 09:29:43 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,17 @@
+set1={10,20,30,40,50} #union
+set2={10,20,60,70,80}
+print(set1.union(set2))
+
+set1={10,20,30,40,50} #intersection
+set2={10,20,60,70,80}
+print(set1.intersection(set2))
+
+set1={10,20,30,40,50} #difference
+set2={10,20,60,70,80}
+print(set1.difference(set2))
+
+set1={10,20,30,40,50} #symmetric difference
+set2={10,20,60,70,80}
+print(set1.symmetric_difference(set2))
+
+
